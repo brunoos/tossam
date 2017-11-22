ff44e4c81c134647f13d734ef85844fab451e95c
Bruno Silvestre <brunoos@inf.ufg.br>
2017-11-22 16:54:05 -0200

    Using coroutines to implement network timeout and resume

--- tossam/network.lua
+++ tossam/network.lua
@@ -1,11 +1,52 @@
 local socket = require("socket")
 
-local function receive(net)
-  return net.conn:receive(1)
+--------------------------------------------------------------------------------
+
+local function lowsend(net, data)
+  local i = 1
+  while true do
+    local succ, msg, last = net.conn:send(data, i)
+    if succ then
+      return true
+    elseif msg ~= "timeout" then
+      return false, msg
+    else
+      if last >= i then
+        i = last + 1
+      end
+      coroutine.yield(false, "timeout")
+    end
+  end
 end
 
+--------------------------------------------------------------------------------
+
 local function send(net, data)
-  return net.conn:send(data)
+  if not data and not net.current then
+    return false, "invalid state"
+  elseif data then
+    net.current = coroutine.create(lowsend)
+  end
+
+  local status, err, msg = coroutine.resume(net.current, net, data)
+
+  if not status then
+    net.current = nil
+    return false, err
+  elseif err then
+    net.current = nil
+    return true
+  elseif msg ~= "timeout" then
+    net.current = nil
+    return false, msg
+  end
+
+  return false, "timeout"
+end
+
+local function receive(net)
+  local  data, err = net.conn:receive(1)
+  return data, err
 end
 
 local function close(net)
